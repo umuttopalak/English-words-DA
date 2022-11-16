5b52c1221e9bd07f325ad54f8d397b83503822c3
Umut Topalak <umuttopalak@hotmail.com>
2022-11-16 23:10:23 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace english_words
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox2.Location = new System.Drawing.Point(46, 114);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(161, 26);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(40, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kelime ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(46, 172);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(556, 178);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(417, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "doğru sayısı : 0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(46, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Bilmiyorum!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(296, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(306, 38);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Kelimeleri dışarı aktar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(41, 452);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(375, 30);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "bilmediğim kelimelerin sayısı : 0";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(692, 518);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vocational Foreign Language";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace english_words
+{
+    public partial class Form1 : Form
+    {
+        
+        public string aciklama;
+        public string key;
+        public int dogruSayisi = 0;
+        Dictionary<string, string> kelimeler = new Dictionary<string, string>();
+
+        public int sayi;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "doğru sayısı : " + dogruSayisi;
+            listBox1.Text = "Bilmediğim kelimeler";
+            
+            string dosyaYolu = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\FormApp\\kelimeler.txt";
+            FileStream fs = new FileStream(dosyaYolu, FileMode.Open, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs);
+            
+            bool bittiMi = false;
+            string kelime;
+            string anlami;
+
+            while (!bittiMi)
+            {
+
+                kelime = sr.ReadLine().ToLower();
+                if (kelime == "bitti")
+                {
+                    bittiMi = true;
+                    break;
+                }
+
+                anlami = sr.ReadLine().ToLower();
+                kelimeler.Add(kelime, anlami);
+
+            }
+
+            sr.Close();
+            Console.WriteLine("ilk kelimeyi yolla");
+            kelimeyiDegistir();
+        }
+
+        private void output()
+        {
+            string outputFilePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Bilmediklerim.txt";
+            StreamWriter sw = new StreamWriter(outputFilePath);
+            foreach (var item in listBox1.Items)
+            {
+                sw.WriteLine(item);
+            }
+
+            sw.Close();
+        }
+
+        private bool IsEnded
+        {
+            get
+            {
+                if (kelimeler.Count == 0)
+                {
+                    return true;
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        private void kelimeyiDegistir()
+        {
+            Random rnd = new Random();
+
+            if(!IsEnded)
+            {
+
+                int rastgeleSayi = rnd.Next(0, kelimeler.Count);
+                label1.Text = kelimeler.ElementAt(rastgeleSayi).Key;
+                aciklama = kelimeler.ElementAt(rastgeleSayi).Value;
+                key = kelimeler.ElementAt(rastgeleSayi).Key;
+                sayi = rastgeleSayi;
+                kelimeler.Remove(kelimeler.ElementAt(sayi).Key);
+            }
+
+            else
+            {
+                string text = "Kelimeler Tükendi!";
+                MessageBox.Show(text);
+                label1.Text = "";
+            }
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e )
+        {
+            if (!IsEnded)
+            {
+           
+                if (richTextBox2.Text.ToLower() == aciklama)
+                {
+                    dogruSayisi++;
+                    label2.Text = "doğru sayısı : " + dogruSayisi;
+                    richTextBox2.Text = "";
+                    kelimeyiDegistir();
+        
+                }   
+            }
+        }
+
+        private void listeyeEkle()
+        {
+            listBox1.Items.Add(aciklama + "  :  " + key);
+            label3.Text = "bilmediğim kelimelerin sayısı : " + Convert.ToInt32(listBox1.Items.Count);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            if (!IsEnded)
+            {
+                listeyeEkle();
+                kelimeyiDegistir();
+            }
+
+            else
+            {
+                string text = "Kelimeler Tükendi!";
+                label1.Text = "";
+                MessageBox.Show(text);
+                
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            output();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+    }
+}
